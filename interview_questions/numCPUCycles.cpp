d439edbce3a218ed766a8e6273fc510f51962197
Kabeer Vamshee <kkvamshee@gmail.com>
2018-11-05 20:00:55 +0530

    GoldmanSachs 2018

--- interview_questions/numCPUCycles.cpp
+++ interview_questions/numCPUCycles.cpp
@@ -1,6 +1,14 @@
 #include<iostream>
 using namespace std;
 
+/*
+You have four integers n,d,c1,c2. Consider a hypothetical computer
+which can perform only two types of operations: (on a given integer x)
+O1 : Subtract 1 from x. This operation costs c1 CPU cycles.
+O2 : Divide x by d. Can be performed only if x is divisible by d. This operation costs c2 CPU cycles.
+What is the minimum number of cycles in which the computer can reduce the input integer from n to 1.
+*/
+
 int numCPUCycles(int n, int d, int c1, int c2) {
     int cost=0;
     while(n>1){
